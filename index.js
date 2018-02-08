c796fce88deb5456003802158be272c7d12b2767
QuangdaoN <quangdaon@gmail.com>
2018-02-08 10:09:39 -0600

    Added IE Check

--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 const ciu = require('caniuse-api');
-//const args = require('yargs')
-//	.usage('Usage: $0 [keyword]')
-//	.alias('v', 'version')
-//	.describe('v', 'Show version number')
-//	.alias('h', 'help')
-//	.example('$0 border-radius', 'Determines if you can use border-radius.')
-//	.epilog('Created by Quangdao Nguyen')
-//	.argv;
 
 const inquirer = require('inquirer');
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
-const appData = require('./package.json');
-
-//if (args.v) {
-//	return appData.version;
-//}
-
-//let keyword = args._[0];
-//if (!keyword) {
-//	return console.error('No keyword provided.');
-//}
-
-//const searchResults = ciu.find(keyword);
-
-function testQuery(q) {
-	return new Promise(function (resolve) {
-		resolve(ciu.find(q || ''));
-	});
+async function ciuSearch(q) {
+	return ciu.find(q || '');
 }
 
-inquirer.prompt([{
-	type: 'autocomplete',
-	name: 'feature',
-	message: 'Select a feature',
-	source: function (answersSoFar, input) {
-		return testQuery(input);
+async function start() {
+	const query = await inquirer.prompt([{
+		type: 'autocomplete',
+		name: 'selected',
+		message: 'Select a feature',
+		source: function (answersSoFar, input) {
+			return ciuSearch(input);
+		}
+	}]);
+
+	const feature = query.selected;
+	let isSupported = false;
+	let browserList = ['> 1%', 'last 2 versions'];
+
+	function testSupport(string) {
+		if (string) browserList.push('not ' + string);
+		isSupported = ciu.isSupported(feature, browserList);
+		return isSupported;
 	}
-}]).then(function (answers) {
-	doThing(answers);
-}).catch(function (err) {
-	console.log(err);
-});
 
-//if (searchResults instanceof Array) {
-//} else {
-//	doThing(searchResults);
-//}
+	if (!testSupport()) {
+		const ieSupport = await inquirer.prompt([
+			{
+				type: 'list',
+				name: 'isNeeded',
+				message: 'Do you need to support Internet Explorer?',
+				choices: ['Yes', 'No'],
+				when() {
+					return !isSupported;
+				},
+				filter(choice) {
+					return choice === 'Yes';
+				}
+			},
+			{
+				type: 'list',
+				name: 'minimum',
+				message: 'You poor soul! What is the minimum version you need to support?',
+				choices: ['9', '10', '11'],
+				when(answers) {
+					return answers.isNeeded;
+				},
+				filter(choice) {
+					return '< ' + choice;
+				}
+			}
+		]);
 
-function doThing(feature) {
-	console.log(feature);
+		if (ieSupport.isNeeded) {
+			testSupport(ieSupport.minimum);
+		} else {
+			testSupport('ie <= 11');
+		}
+
+	}
+
+	ciu.setBrowserScope(browserList);
+	if (isSupported) {
+		return `${feature} is safe to use!`;
+	} else {
+		return ciu.getSupport(feature);//`${feature} is not well supported enough for your requirements...`;
+	}
+
 }
+
+start().then(console.log).catch(function (e) {
+	console.log(e);
+});
